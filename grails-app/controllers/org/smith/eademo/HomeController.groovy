98f9a578caf160e547c48e59d1b4fe3a52dfbd54
Leighton Smith <leightdude@gmail.com>
2013-01-19 11:18:38 -0800

    Secured the home controller.

--- grails-app/controllers/org/smith/eademo/HomeController.groovy
+++ grails-app/controllers/org/smith/eademo/HomeController.groovy
@@ -1,8 +1,8 @@
 package org.smith.eademo
 
-// import grails.plugins.springsecurity.Secured
+import grails.plugins.springsecurity.Secured
 
+@Secured(['ROLE_USER','ROLE_ADMIN'])
 class HomeController {
-
     def index() { }
 }
