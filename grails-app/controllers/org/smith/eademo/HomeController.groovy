508fcf2e2f5c0a0f92062c8dc196a9215d93467e
Leighton Smith <leightdude@gmail.com>
2013-01-19 11:05:12 -0800

    Added the security plugin, and a home controller.

---
+++ grails-app/controllers/org/smith/eademo/HomeController.groovy
@@ -0,0 +1,8 @@
+package org.smith.eademo
+
+// import grails.plugins.springsecurity.Secured
+
+class HomeController {
+
+    def index() { }
+}
---
+++ grails-app/domain/org/smith/eademo/Role.groovy
@@ -0,0 +1,14 @@
+package org.smith.eademo
+
+class Role {
+
+	String authority
+
+	static mapping = {
+		cache true
+	}
+
+	static constraints = {
+		authority blank: false, unique: true
+	}
+}
